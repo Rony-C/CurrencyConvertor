bbea5992f2e1af7d67c0e0dce841b97c63105344
Ronan <53212845+Rony-C@users.noreply.github.com>
2023-03-26 13:50:23 +0100

    Added input validation
    Checks if ISO Code is three characters long
    Checks if amount entered is a number. Need to add proper exception handling

--- Menu.java
+++ Menu.java
@@ -1,25 +1,54 @@
-package org.example;
+package org.ronan;
 
 import java.util.Scanner;
 
 public class Menu {
-    static void start(){
+    static Scanner sc = new Scanner(System.in);
+
+    static String isISOCode() {
+        String iso = "";
+
+        while (iso.length() != 3) {
+            iso = sc.next();
+            if (iso.length() == 3) {
+                return iso;
+            } else {
+                System.out.println("[INFO] " + iso + " is not a valid ISO code.");
+            }
+        }
+        return iso;
+    }
+
+    static double isNumber() {
+        double amount = 0;
+
+        while (sc.hasNextDouble()) {
+            amount = sc.nextDouble();
+            if (amount > 0) {
+                return amount;
+            } else {
+                System.out.println("[INFO] " + amount + " needs to be greater than zero/ 0.");
+            }
+        }
+        return amount;
+    }
+
+    static void start() {
         System.out.println("""
                 Currency convertor
                 Use three letter ISO currency code
                 eg. "USD", "EUR", "CAD"
                 """);
 
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("What currency do you want to convert from? ");
-        String convertFrom = sc.next();
+
+        String convertFrom = isISOCode();
 
         System.out.println("What currency do you want to convert to?");
-        String convertTo = sc.next();
+        String convertTo = isISOCode();
 
         System.out.println("How much do you want to convert? ");
-        double amount = sc.nextDouble();
+        double amount = isNumber();
 
         CurrencyConvertor cc = new CurrencyConvertor();
         cc.convert(convertTo, convertFrom, amount);
